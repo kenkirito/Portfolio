707b8de715f9c13a3b28298b64232974da34914a
kenkirito <lakshya.mishra56@gmail.com>
2021-07-11 14:53:15 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
-
+import { IconButton } from "@chakra-ui/button";
+import { Flex, VStack, Heading, Spacer } from "@chakra-ui/react";
+import { FaSun, FaMoon, FaGithub, FaLinkedin} from 'react-icons/fa';
+import { useColorMode } from "@chakra-ui/color-mode";
+import Header from "./components/Header";
+import Profile from "./components/Profile";
+import Social from "./components/Social";
 
 function App() {
+
+  const { colorMode, toggleColorMode } = useColorMode();
+  const isDark = colorMode === "dark";
+
   return (
-    <div className="App">
-      pk app
-    </div>
+    
+      <VStack p={5}>
+        <Flex w="100%">
+        <Heading
+        ml="8" size="md" fontWeight="semibold" color="cyan.400"
+         >
+         Lakshya's Portfolio
+        </Heading>
+        <Spacer></Spacer>
+        <IconButton ml={2} icon={ <FaGithub /> } isRound="true" ></IconButton>
+        <IconButton ml={2} icon={ <FaLinkedin /> } isRound="true"></IconButton>
+        <IconButton ml={8} icon={isDark ? <FaSun /> : <FaMoon />} isRound="true" onClick={toggleColorMode}></IconButton>
+        </Flex>
+        <Header></Header>
+        <Social></Social>
+        <Profile></Profile>
+      </VStack>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Stack, Circle, Flex, Box, Text } from '@chakra-ui/layout';
+import {useMediaQuery} from '@chakra-ui/media-query';
+import { useColorMode } from "@chakra-ui/color-mode";
+import { Image } from '@chakra-ui/image';
+import { Button } from '@chakra-ui/button';
+
+function Header() {
+    const { colorMode, toggleColorMode } = useColorMode();
+    const isDark = colorMode === "dark";
+
+    const [isNotSmallerScreen] = useMediaQuery("(min-width: 600px)");
+    return (
+        <Stack>
+         <Circle position="absolute" bg="blue.100" opacity="0.02" w="300px" h="300px" alignSelf="flex-end" 
+         />
+         <Flex direction={isNotSmallerScreen ? "row" : "column" }
+         spacing="200px" p={isNotSmallerScreen ? "32" : "0"}
+         alignSelf="flex-start">
+            <Box p={isNotSmallerScreen ? "0" : "16"}>
+                <Text fontSize="5xl" fontWeight="semibold"> Hi, i am  </Text>
+                <Text fontSize="7xl" fontWeight="bold" bgGradient="linear(to-r, cyan.400, blue.500, purple.600)" bgClip="text" > Lakshya Mishra </Text>
+                <Text color={isDark ? "gray.200" : "gray.500"}> mern stack, python, flutter, reactnative, devOps, threejs, Blenders 🍥🍥🍥</Text>
+                <Button mt={8} colorScheme="blue" >Hire Me</Button>
+            </Box>
+            <Image alignSelf="center" mt={isNotSmallerScreen ? "0" : "12"}
+                    mb={isNotSmallerScreen ? "0" : "12"} borderRadius='full'
+                     boxShadow="lg" backgroundColor="transparent"
+                     boxSize="300px"
+             src='https://avatars.githubusercontent.com/u/68530218?s=400&u=96d067e5bda86c33d465812e98380a14b071b89f&v=4' />
+
+         </Flex>
+        </Stack>
+    )
+}
+
+export default Header
+
