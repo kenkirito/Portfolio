72f3b9697868a130933ca2044dba93b914911991
kenkirito <lakshya.mishra56@gmail.com>
2021-07-12 05:36:55 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { IconButton } from "@chakra-ui/button";
 import { Flex, VStack, Heading, Spacer } from "@chakra-ui/react";
-import { FaSun, FaMoon, FaGithub, FaLinkedin} from 'react-icons/fa';
+import { FaSun, FaMoon, FaGithub, FaLinkedin, FaYoutube, FaTwitter} from 'react-icons/fa';
 import { useColorMode } from "@chakra-ui/color-mode";
 import Header from "./components/Header";
 import Profile from "./components/Profile";
@@ -30,6 +30,14 @@ function App() {
         <IconButton ml={2} icon={ <FaLinkedin /> } isRound="true"
          onClick={() =>
                         window.open("https://www.linkedin.com/in/lakshya-mishra-61aa33191/")}
+        ></IconButton>
+        <IconButton ml={2} icon={ <FaYoutube /> } isRound="true"
+         onClick={() =>
+                        window.open("https://www.youtube.com/channel/UCmyEipN5pT6sxp1h2tX9xNA")}
+        ></IconButton>
+        <IconButton ml={2} icon={ <FaTwitter /> } isRound="true"
+         onClick={() =>
+                        window.open("https://twitter.com/LakshyaMishra56")}
         ></IconButton>
         <IconButton ml={8} icon={isDark ? <FaSun /> : <FaMoon />} isRound="true" onClick={toggleColorMode}></IconButton>
         </Flex>
