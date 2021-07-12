3eee18105b2e9d2ce1c8c99a75bf2d507fcd6e1b
kenkirito <lakshya.mishra56@gmail.com>
2021-07-12 06:11:24 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,42 +1,57 @@
 import { IconButton } from "@chakra-ui/button";
 import { Flex, VStack, Heading, Spacer } from "@chakra-ui/react";
-import { FaSun, FaMoon, FaGithub, FaLinkedin, FaStackOverflow, FaYoutube, FaTwitter} from 'react-icons/fa';
+import {
+  FaSun,
+  FaMoon,
+  FaGithub,
+  FaLinkedin,
+  FaStackOverflow,
+  FaYoutube,
+  FaTwitter,
+} from "react-icons/fa";
 import { useColorMode } from "@chakra-ui/color-mode";
 import Header from "./components/Header";
 import Profile from "./components/Profile";
 import Social from "./components/Social";
 
 function App() {
-
   const { colorMode, toggleColorMode } = useColorMode();
   const isDark = colorMode === "dark";
 
   return (
-    
-      <VStack p={5}>
-        <Flex w="100%">
-        <Heading
-        ml="8" size="md" fontWeight="semibold" color="cyan.400"
-         >
-         Lakshya's Portfolio
+    <VStack p={5}>
+      <Flex w="100%">
+        <Heading ml="8" size="md" fontWeight="semibold" color="cyan.400">
+          Lakshya's Portfolio
         </Heading>
         <Spacer></Spacer>
-        
-        <IconButton ml={2} icon={ <FaGithub /> } isRound="true"
-        onClick={() =>
-                        window.open("https://github.com/kenkirito")}
-         ></IconButton>
 
-<IconButton ml={2} icon={ <FaStackOverflow /> } isRound="true"
-        onClick={() =>
-                        window.open("https://stackoverflow.com/users/16427979/lakshya")}
-         ></IconButton>         
-        <IconButton ml={8} icon={isDark ? <FaSun /> : <FaMoon />} isRound="true" onClick={toggleColorMode}></IconButton>
-        </Flex>
-        <Header />
-        <Social />
-        <Profile />
-      </VStack>
+        <IconButton
+          ml={2}
+          icon={<FaGithub />}
+          isRound="true"
+          onClick={() => window.open("https://github.com/kenkirito")}
+        ></IconButton>
+
+        <IconButton
+          ml={2}
+          icon={<FaStackOverflow />}
+          isRound="true"
+          onClick={() =>
+            window.open("https://stackoverflow.com/users/16427979/lakshya")
+          }
+        ></IconButton>
+        <IconButton
+          ml={8}
+          icon={isDark ? <FaSun /> : <FaMoon />}
+          isRound="true"
+          onClick={toggleColorMode}
+        ></IconButton>
+      </Flex>
+      <Header />
+      <Social />
+      <Profile />
+    </VStack>
   );
 }
 
--- src/components/Social.js
+++ src/components/Social.js
@@ -1,29 +1,51 @@
-import Icon from '@chakra-ui/icon'
-import { HStack } from '@chakra-ui/layout'
-import React from 'react'
-import { FaTwitter, FaLinkedinIn, FaGoogle, FaYoutube, FaSpotify, FaMailBulk, FaWhatsapp } from 'react-icons/fa'
+import Icon from "@chakra-ui/icon";
+import { HStack } from "@chakra-ui/layout";
+import React from "react";
+import {
+  FaTwitter,
+  FaLinkedinIn,
+  FaGoogle,
+  FaYoutube,
+  FaSpotify,
+  FaMailBulk,
+  FaWhatsapp,
+} from "react-icons/fa";
 
 function Social() {
-    return (
-        <HStack spacing="24">
-            <Icon as={FaTwitter} style={{cursor: 'pointer'}} boxSize="50" 
-            onClick={() =>
-                        window.open("https://twitter.com/LakshyaMishra56")}
-             />
-            <Icon as={FaLinkedinIn} style={{cursor: 'pointer'}} boxSize="50" 
-                 onClick={() =>
-                        window.open("https://www.linkedin.com/in/lakshya-mishra-61aa33191/")}
-            />
-            <Icon as={FaYoutube}   style={{cursor: 'pointer'}}
-             onClick={() =>
-                        window.open("https://www.youtube.com/channel/UCmyEipN5pT6sxp1h2tX9xNA")}
-            boxSize="50" />
-            <Icon as={FaMailBulk} boxSize="50"  style={{cursor: 'pointer'}}
-             onClick={() =>
-                        window.open("mailto:lakshya.mishra56@gmail.com")}
-            />
-        </HStack>
-    )
+  return (
+    <HStack spacing="24">
+      <Icon
+        as={FaTwitter}
+        style={{ cursor: "pointer" }}
+        boxSize="50"
+        onClick={() => window.open("https://twitter.com/LakshyaMishra56")}
+      />
+      <Icon
+        as={FaLinkedinIn}
+        style={{ cursor: "pointer" }}
+        boxSize="50"
+        onClick={() =>
+          window.open("https://www.linkedin.com/in/lakshya-mishra-61aa33191/")
+        }
+      />
+      <Icon
+        as={FaYoutube}
+        style={{ cursor: "pointer" }}
+        onClick={() =>
+          window.open(
+            "https://www.youtube.com/channel/UCmyEipN5pT6sxp1h2tX9xNA"
+          )
+        }
+        boxSize="50"
+      />
+      <Icon
+        as={FaMailBulk}
+        boxSize="50"
+        style={{ cursor: "pointer" }}
+        onClick={() => window.open("mailto:lakshya.mishra56@gmail.com")}
+      />
+    </HStack>
+  );
 }
 
-export default Social
+export default Social;
